3780b22ddd7134c0c714b7279aaf41678c2a4ae5
Jesse Beck <thewebbeckons@users.noreply.github.com>
2024-12-04 18:16:42 -0500

    add nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/fonts", "@vueuse/nuxt", "@nuxtjs/supabase"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@vueuse/nuxt",
+    "@nuxtjs/supabase",
+    "@nuxthub/core",
+  ],
   supabase: {
     redirectOptions: {
       login: "/login",
